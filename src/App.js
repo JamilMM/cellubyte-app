cbede9720eb9b8b3b7e4026b8e1052a0d42d6daf
jamilMM <mahamajamil1@gmail.com>
2021-12-21 12:30:00 +0000

    first update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
+import bgImage from './video/worldconnect.mp4'
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   return(
+     <div className="App">
+      <div className='text-wrapper'>
+      <video autoPlay loop muted>
+           <source src={bgImage} type="video/mp4" />
+        </video>
+         <h1 className="title"> CELLUBYTE</h1>
+         <p>Cellubyte is a simple and easy to use network coverage map for your 5G device</p>
+         <p>Official Website Launching Early 2022</p>
+      </div>
+     </div>
+   );
 }
 
 export default App;
